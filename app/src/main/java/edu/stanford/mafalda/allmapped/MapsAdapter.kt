9f5ee608ca551f2907b54f27542e6850b0ab884b
mafalda-b <mafalda.m.m.barros@gmail.com>
2020-05-11 16:32:29 -0700

    Third commit - added instructions for the user

---
+++ app/src/main/java/edu/stanford/mafalda/allmapped/MapsAdapter.kt
@@ -0,0 +1,39 @@
+package edu.stanford.mafalda.allmapped
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import edu.stanford.mafalda.allmapped.models.UserMap
+
+private const val TAG = "MapsAdapter"
+class MapsAdapter(val context: Context, val userMaps: List<UserMap>, val onClickListener: OnClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    // make the MainActivity know that the MapsAdapter has recorded activity with the ClickListener to then be able to react - to that we create an interface
+    interface OnClickListener {
+        fun onItemClick (position: Int)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = userMaps.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val userMap = userMaps[position]
+        holder.itemView.setOnClickListener{
+            Log.i (TAG, "Tapped on position $position")
+            onClickListener.onItemClick(position)
+        }
+        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
+        textViewTitle.text = userMap.title
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+}
