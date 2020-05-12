69accf920fceafe7578dbb64bc276c4c3f67f957
mafalda-b <mafalda.m.m.barros@gmail.com>
2020-05-11 18:03:30 -0700

    Fourth commit - user is able to save a new map containing several locations

--- app/src/main/java/edu/stanford/mafalda/allmapped/CreateMapActivity.kt
+++ app/src/main/java/edu/stanford/mafalda/allmapped/CreateMapActivity.kt
@@ -1,10 +1,14 @@
 package edu.stanford.mafalda.allmapped
 
+import android.app.Activity
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -18,6 +22,8 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.snackbar.Snackbar
+import edu.stanford.mafalda.allmapped.models.Place
+import edu.stanford.mafalda.allmapped.models.UserMap
 
 private const val TAG = "CreateMapActivity"
 class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -40,6 +46,30 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
                 .setActionTextColor(ContextCompat.getColor(this,android.R.color.white))
                 .show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_create_map, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Check that "item" is the save menu option
+        if(item.itemId == R.id.miSave) {
+            Log.i(TAG, "Tapped on save!")
+            if (markers.isEmpty()) {
+                Toast.makeText(this, "You have to put a marker first", Toast.LENGTH_LONG).show()
+                return true
+            }
+            val places = markers.map {marker -> Place(marker.title, marker.snippet, marker.position.latitude, marker.position.longitude) }
+            val userMap = UserMap(intent.getStringExtra(EXTRA_MAP_TITLE), places)
+            val data = Intent()
+            data.putExtra(EXTRA_USER_MAP, userMap)
+            setResult(Activity.RESULT_OK, data)
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     /**
@@ -66,10 +96,10 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
         }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Start at home (but don't save the place in the map
+        val home = LatLng(37.427852, -122.153943)
+//        mMap.addMarker(MarkerOptions().position(home).title("Stanford Home"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 15f))
     }
 
     private fun showAlertDialogue(latLng: LatLng) {
--- app/src/main/java/edu/stanford/mafalda/allmapped/MainActivity.kt
+++ app/src/main/java/edu/stanford/mafalda/allmapped/MainActivity.kt
@@ -1,11 +1,17 @@
 package edu.stanford.mafalda.allmapped
 
 import android.app.Activity
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.maps.model.LatLng
 import edu.stanford.mafalda.allmapped.models.Place
 import edu.stanford.mafalda.allmapped.models.UserMap
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,17 +22,20 @@ private const val REQUEST_CODE = 1234
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var userMaps: MutableList<UserMap>
+    private lateinit var mapAdapter: MapsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userMaps = generateSampleData()
+        userMaps = generateSampleData().toMutableList()
         // set layout manager on recycler view
-        rvMaps.layoutManager = LinearLayoutManager ( this)
+        rvMaps.layoutManager = LinearLayoutManager(this)
         // ser adapter on recycler view
-        rvMaps.adapter = MapsAdapter(this, userMaps, object: MapsAdapter.OnClickListener {
+        mapAdapter = MapsAdapter(this, userMaps, object : MapsAdapter.OnClickListener {
             override fun onItemClick(position: Int) {
-                Log.i (TAG,"OnItemClick $position")
+                Log.i(TAG, "OnItemClick $position")
                 // when user taps on view in Recycler View, navigate to new activity
                 val intent = Intent(this@MainActivity, DisplayMapActivity::class.java)
                 intent.putExtra(EXTRA_USER_MAP, userMaps[position])
@@ -34,63 +43,145 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+        rvMaps.adapter = mapAdapter
+
         fabCreateMap.setOnClickListener {
             Log.i(TAG, "Tap on FAB")
-            val intent = Intent(this@MainActivity, CreateMapActivity::class.java )
-            intent.putExtra(EXTRA_MAP_TITLE, "new map name")
+            showAlertDialogue()
+
+        }
+    }
+
+    private fun showAlertDialogue() {
+        val mapFormView = LayoutInflater.from(this).inflate(R.layout.dialog_create_map, null)
+        val dialog =
+            AlertDialog.Builder(this)
+                .setTitle("Map title")
+                .setView(mapFormView)
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("OK", null)
+                .show()
+
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            // validate the content before dismissing dialogue box
+            val title = mapFormView.findViewById<EditText>(R.id.etTitle).text.toString()
+            if (title.trim().isEmpty()) {
+                Toast.makeText(this, "Please indicate title of the map", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            // Navigate to create map activity
+            val intent = Intent(this@MainActivity, CreateMapActivity::class.java)
+            intent.putExtra(EXTRA_MAP_TITLE, title)
             startActivityForResult(intent, REQUEST_CODE)
+            dialog.dismiss()
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            // Get new data from the app
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+                // Get new map data from the data
+                val userMap = data?.getSerializableExtra(EXTRA_USER_MAP) as UserMap
+                Log.i(TAG, "onActivityResult with new data ${userMap.title}")
+                userMaps.add(userMap)
+                mapAdapter.notifyItemInserted(userMaps.size - 1)
+            }
+            super.onActivityResult(requestCode, resultCode, data)
         }
-        super.onActivityResult(requestCode, resultCode, data)
-    }
 
-    private fun generateSampleData(): List<UserMap> {
-        return listOf(
-            UserMap(
-                "Memories from University",
-                listOf(
-                    Place("Branner Hall", "Best dorm at Stanford", 37.426, -122.163),
-                    Place("Gates CS building", "Many long nights in this basement", 37.430, -122.173),
-                    Place("Pinkberry", "First date with my wife", 37.444, -122.170)
-                )
-            ),
-            UserMap("January vacation planning!",
-                listOf(
-                    Place("Tokyo", "Overnight layover", 35.67, 139.65),
-                    Place("Ranchi", "Family visit + wedding!", 23.34, 85.31),
-                    Place("Singapore", "Inspired by \"Crazy Rich Asians\"", 1.35, 103.82)
-                )),
-            UserMap("Singapore travel itinerary",
-                listOf(
-                    Place("Gardens by the Bay", "Amazing urban nature park", 1.282, 103.864),
-                    Place("Jurong Bird Park", "Family-friendly park with many varieties of birds", 1.319, 103.706),
-                    Place("Sentosa", "Island resort with panoramic views", 1.249, 103.830),
-                    Place("Botanic Gardens", "One of the world's greatest tropical gardens", 1.3138, 103.8159)
-                )
-            ),
-            UserMap("My favorite places in the Midwest",
-                listOf(
-                    Place("Chicago", "Urban center of the midwest, the \"Windy City\"", 41.878, -87.630),
-                    Place("Rochester, Michigan", "The best of Detroit suburbia", 42.681, -83.134),
-                    Place("Mackinaw City", "The entrance into the Upper Peninsula", 45.777, -84.727),
-                    Place("Michigan State University", "Home to the Spartans", 42.701, -84.482),
-                    Place("University of Michigan", "Home to the Wolverines", 42.278, -83.738)
-                )
-            ),
-            UserMap("Restaurants to try",
-                listOf(
-                    Place("Champ's Diner", "Retro diner in Brooklyn", 40.709, -73.941),
-                    Place("Althea", "Chicago upscale dining with an amazing view", 41.895, -87.625),
-                    Place("Shizen", "Elegant sushi in San Francisco", 37.768, -122.422),
-                    Place("Citizen Eatery", "Bright cafe in Austin with a pink rabbit", 30.322, -97.739),
-                    Place("Kati Thai", "Authentic Portland Thai food, served with love", 45.505, -122.635)
+        private fun generateSampleData(): List<UserMap> {
+            return listOf(
+                UserMap(
+                    "Memories from University",
+                    listOf(
+                        Place("Branner Hall", "Best dorm at Stanford", 37.426, -122.163),
+                        Place(
+                            "Gates CS building",
+                            "Many long nights in this basement",
+                            37.430,
+                            -122.173
+                        ),
+                        Place("Pinkberry", "First date with my wife", 37.444, -122.170)
+                    )
+                ),
+                UserMap(
+                    "January vacation planning!",
+                    listOf(
+                        Place("Tokyo", "Overnight layover", 35.67, 139.65),
+                        Place("Ranchi", "Family visit + wedding!", 23.34, 85.31),
+                        Place("Singapore", "Inspired by \"Crazy Rich Asians\"", 1.35, 103.82)
+                    )
+                ),
+                UserMap(
+                    "Singapore travel itinerary",
+                    listOf(
+                        Place("Gardens by the Bay", "Amazing urban nature park", 1.282, 103.864),
+                        Place(
+                            "Jurong Bird Park",
+                            "Family-friendly park with many varieties of birds",
+                            1.319,
+                            103.706
+                        ),
+                        Place("Sentosa", "Island resort with panoramic views", 1.249, 103.830),
+                        Place(
+                            "Botanic Gardens",
+                            "One of the world's greatest tropical gardens",
+                            1.3138,
+                            103.8159
+                        )
+                    )
+                ),
+                UserMap(
+                    "My favorite places in the Midwest",
+                    listOf(
+                        Place(
+                            "Chicago",
+                            "Urban center of the midwest, the \"Windy City\"",
+                            41.878,
+                            -87.630
+                        ),
+                        Place(
+                            "Rochester, Michigan",
+                            "The best of Detroit suburbia",
+                            42.681,
+                            -83.134
+                        ),
+                        Place(
+                            "Mackinaw City",
+                            "The entrance into the Upper Peninsula",
+                            45.777,
+                            -84.727
+                        ),
+                        Place("Michigan State University", "Home to the Spartans", 42.701, -84.482),
+                        Place("University of Michigan", "Home to the Wolverines", 42.278, -83.738)
+                    )
+                ),
+                UserMap(
+                    "Restaurants to try",
+                    listOf(
+                        Place("Champ's Diner", "Retro diner in Brooklyn", 40.709, -73.941),
+                        Place(
+                            "Althea",
+                            "Chicago upscale dining with an amazing view",
+                            41.895,
+                            -87.625
+                        ),
+                        Place("Shizen", "Elegant sushi in San Francisco", 37.768, -122.422),
+                        Place(
+                            "Citizen Eatery",
+                            "Bright cafe in Austin with a pink rabbit",
+                            30.322,
+                            -97.739
+                        ),
+                        Place(
+                            "Kati Thai",
+                            "Authentic Portland Thai food, served with love",
+                            45.505,
+                            -122.635
+                        )
+                    )
                 )
             )
-        )
+        }
     }
-}
+
